8030a953166a0af629d113d3f35b1ffeecc9ed39
bingining <bingining@gmail.com>
2020-02-14 00:32:08 +0000

    update doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,13 @@
 using Documenter, GWSC
 
 makedocs(
-    ;
+    sitename = "GWSC.jl",
     modules = [GWSC],
-    format = Documenter.HTML(),
     pages = ["Home" => "index.md",],
     repo = "https://github.com/bingining/GWSC.jl/blob/{commit}{path}#L{line}",
-    sitename = "GWSC.jl",
     authors = "Zu-Cheng Chen",
     assets = String[],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"), # easier local build
 )
 
-deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/bingining/GWSC.jl.git",
-)
+deploydocs(repo = "github.com/bingining/GWSC.jl.git", push_preview = true)
