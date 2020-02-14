96ea53bf35d315a87ddbc3552fb8d1f976e57eee
bingining <bingining@gmail.com>
2020-02-14 00:47:29 +0000

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,12 @@
 using Documenter, GWSC
 
 makedocs(
+    doctest = false,
     sitename = "GWSC.jl",
     modules = [GWSC],
     pages = ["Home" => "index.md",],
     repo = "https://github.com/bingining/GWSC.jl/blob/{commit}{path}#L{line}",
     authors = "Zu-Cheng Chen",
-    assets = String[],
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"), # easier local build
 )
 
